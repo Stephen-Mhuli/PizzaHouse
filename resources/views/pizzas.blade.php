c9fd90d987e3848dfd3a922f1d4268fa9d9ec71d
stevemshible <stevemshible@gmail.com>
2021-09-03 00:24:14 +0300

    blade syntax basics

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -70,6 +70,22 @@
                     Pizza List
                 </div>
                 <p>{{ $type }} - {{ $base }} - {{ $price }}</p>
+                @if($price>=15)
+                   <p>This pizza is expensive</p>
+                @elseif($price<=5)
+                    <p>This pizza is cheap</p>   
+                @else
+                   <p>This pizza is normally priced</p>    
+                @endif
+                
+                @unless($type=='Hawaian crust')
+                   <p>You don't have hawaian</p>
+                @endunless
+
+                @php
+                  $name = 'Taylor';
+                  echo $name;
+                @endphp
             </div>
         </div>
     </body>
